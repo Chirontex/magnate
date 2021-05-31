4d369e46d9dd0da8f0c8dd39301b1712a3bbab92
Dmitry Shumilin <infernozzz@yandex.ru>
2021-05-31 23:08:03 +0300

    comlete testing stand; add tests autoloader

---
+++ tests/autoload-tests.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package Magnate
+ * 
+ * @since 0.9.8
+ */
+spl_autoload_register(function($classname) {
+
+    if (strpos($classname, 'Magnate\\Test') !== false) {
+
+        $file = explode('\\', $classname);
+        
+        if ($file[0] !== 'Magnate' ||
+        $file[1] !== 'Test') return;
+        
+        $path = __DIR__.'/';
+
+        for ($i = 2; $i < count($file) - 1; $i++) {
+
+            $path .= $file[$i].'/';
+
+        }
+
+        $path .= $file[count($file) - 1].'.php';
+
+        if (file_exists($path)) require_once $path;
+
+    }
+
+});
