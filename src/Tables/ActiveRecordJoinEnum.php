fa729f1855002c5dd5a569988f319e5a5bc9fb3e
Dmitry Shumilin <infernozzz@yandex.ru>
2021-06-20 23:19:27 +0300

    add join enum

---
+++ src/Tables/ActiveRecordJoinEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Magnate\Tables;
+
+class ActiveRecordJoinEnum
+{
+
+    public const INNER_JOIN = 'INNER JOIN';
+    public const FULL_JOIN = 'FULL JOIN';
+    public const RIGHT_JOIN = 'RIGHT JOIN';
+    public const LEFT_JOIN = 'LEFT JOIN';
+    public const CROSS_JOIN = 'CROSS JOIN';
+
+}
